005ebe239abefd514fd5a9cd11b91e7213f05527
GermanBluefox <dogafox@gmail.com>
2013-12-26 18:36:56 +0100

    - Add jQueryUI for DatePicker

--- js/eventlist.js
+++ js/eventlist.js
@@ -49,7 +49,7 @@ var eventlist;
 			vtrue:       null,   // Replace true with this value
 			vfalse:      null    // Replace false with this value
         },
-        version:     "0.0.16",
+        version:     "0.0.17",
         requiredCcuIoVersion: "0.9.62",
         socket:      null,
         regaObjects: null,
